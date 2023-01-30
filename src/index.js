a22c59ab0bd8d2aa1342362808fb0bf68b2e5c29
Jennyver Seztiani Luxman <114470554+JenJenFenFen@users.noreply.github.com>
2023-01-30 20:49:59 +0700

    new repository

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv').config();
 
 const router = require('./routes/router');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ 
+    extended: true 
+}));
 app.use(express.json());
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ 
+    origin: true,
+    credentials: true
+}));
 
 app.use('/', router);
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log('Server Running')
+    console.log(`Listening port ${process.env.SERVER_PORT}`)
 });
